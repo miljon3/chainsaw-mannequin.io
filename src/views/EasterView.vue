3992848d7b6bdc00ad323ba31a4c822ea6973def
Carl <e12341573@student.tuwien.ac.at>
2024-10-12 19:03:57 +0200

    newview

---
+++ src/views/EasterView.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="about">
+      <h1>    /\    /\
+            /  \  /  \
+            / /\ \/ /\ \
+            / /  \ \/  \ \
+            / /   /\ \   \ \
+            \ \  / /\ \  / /
+            \ \/ /  \ \/ /
+            \/ /    \/ /
+            / /\    / /\
+            / /\ \  / /\ \
+            / /  \ \/ /  \ \
+            \ \   \ \/   / /
+            \ \  /\ \  / /
+            \ \/ /\ \/ /
+            \  /  \  /
+                \/    \/</h1>
+    </div>
+  </template>
+  
+  <style>
+  @media (min-width: 1024px) {
+    .about {
+      min-height: 100vh;
+      display: flex;
+      align-items: center;
+    }
+  }
+  </style>
+  
