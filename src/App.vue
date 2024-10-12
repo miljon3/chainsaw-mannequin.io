0db1d801ae565c799d56b6b67b64c49d129e53ee
Carl <e12341573@student.tuwien.ac.at>
2024-10-12 19:06:31 +0200

    easter

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/easter">Easter</RouterLink>
       </nav>
     </div>
   </header>
