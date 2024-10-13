979e0056858024d10317d3594b1fce551204e7d5
Carl <e12341573@student.tuwien.ac.at>
2024-10-13 11:36:00 +0200

    changes for final tutorial

--- src/router/index.js
+++ src/router/index.js
@@ -3,21 +3,20 @@ import { createRouter, createWebHashHistory } from 'vue-router';
 // Automatically import all views from the "views" directory
 const viewModules = import.meta.glob('../views/*.vue');
 
-// Create a function to map these view modules to routes
-const routes = Object.keys(viewModules).map((viewPath) => {
+export const routes = Object.keys(viewModules).map((viewPath) => {
   const name = viewPath.split('/').pop().replace('.vue', '');
-  const path = name === 'HomeView' ? '/' : `/${name.toLowerCase()}`;
-  
+  const path = name === 'HomeView' ? '/' : `/${name.replace('View', '').toLowerCase()}`;
+
   return {
     path: path,
     name: name.toLowerCase(),
-    component: viewModules[viewPath] // Lazy-loaded components
+    component: viewModules[viewPath], // Lazy-loaded components
   };
 });
 
 const router = createRouter({
   history: createWebHashHistory(import.meta.env.BASE_URL),
-  routes
+  routes,
 });
 
 export default router;
